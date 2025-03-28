7f7ec839589f8ba0e58f166a857cb8977cb2411f
Matthew Barhou <mbarhou@my.centennialcollege.ca>
2025-03-28 15:40:23 -0400

    Added test project for the string lib package

--- StringLibrary/obj/Debug/net8.0/StringLibrary.AssemblyInfo.cs
+++ StringLibrary/obj/Debug/net8.0/StringLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StringLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b21f0fd6701181b9802b5dc59cfda6d40c3f7e22")]
 [assembly: System.Reflection.AssemblyProductAttribute("StringLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StringLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ TestStringLibraryExtension/Program.cs
@@ -0,0 +1,14 @@
+﻿using UtilityLibraries;
+
+namespace TestStringLibraryExtension
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = "hello";
+
+            Console.WriteLine($"Is {word} uppercase? The answer is: {word.StartsWithUpper()}");
+        }
+    }
+}
